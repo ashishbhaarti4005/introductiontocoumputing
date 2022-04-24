64c2ffe9ecb7a8bbf9b7e1552c3ed2be04d27e7f
Ashish bharti <98837841+ashishbhaarti4005@users.noreply.github.com>
2022-04-24 15:58:03 +0530

    Add files via upload
    ASHISH BHARTI 21102012 CIVIL

---
+++ answer 21102012.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/ashis/OneDrive/Desktop/ass question codes.py ========
+Answer of question number 1
+firstnumber=4
+second number=5
+third number=6
+Average= 5.0
+ 
+Answer of question number 2:
+Gross Income:4000
+no. of famiy members:3
+Tax= -1201.0
+ 
+Answer of question number 3
+Student
+SID:21102012
+Name:ashish bharti
+Gender:m
+Course Name:ma1301
+CGPA:9.2
+f [21102012, 'ashish bharti', 'm', 'ma1301', 9.2]
+ 
+Answer of question number 4
+Marks of 1st student:78
+Marks of 2nd student:89
+Marks of 3rd student:78
+Marks of 4th student:90
+Marks of 5th student:95
+sortedmarks [78, 89, 78, 90, 95]
+ 
+Answer of question number 5
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'pink', 'yellow']
+['red', 'green', 'white', 'puple', 'purple', 'yellow']
---
+++ question 21102012.py
@@ -0,0 +1,70 @@
+#question number 1:............................................................................................................................
+print("Answer of question number 1")
+
+a=float(input("firstnumber="))
+b=float(input("second number="))
+c=float(input("third number="))
+d=(a+b+c)/3
+print("Average=",d)
+
+
+
+#question number 2:.............................................................................................................................
+print(" ")
+print("Answer of question number 2:")
+
+
+a=float(input("Gross Income:"))
+b=float(input("no. of famiy members:"))
+c=3000*b
+d=a-10000-(d)
+e=(d*20)/100
+print("Tax=",e)
+
+
+
+#question number 3:..............................................................................................................................
+print(" ")
+print("Answer of question number 3")
+
+print("Student")
+a=int(input("SID:"))
+b=str(input("Name:"))
+c=str(input("Gender:"))
+d=str(input("Course Name:"))
+e=float(input("CGPA:"))
+f=[a,b,c,d,e]
+print("f",f)
+
+
+
+#question number 4:................................................................................................................................
+print(" ")
+print("Answer of question number 4")
+
+
+a=int(input("Marks of 1st student:"))
+b=int(input("Marks of 2nd student:"))
+c=int(input("Marks of 3rd student:"))
+d=int(input("Marks of 4th student:"))
+e=int(input("Marks of 5th student:"))
+sortedmarks=[a,b,c,d,e]
+print("sortedmarks",sortedmarks)
+
+
+
+#question number 5:................................................................................................................................
+print(" ")
+print("Answer of question number 5")
+
+#part 1
+color=['red','green','white','black','pink','yellow']
+print(color)
+color.remove('black')
+print(color)
+
+#part2
+color=['red','green','white','black','pink','yellow']
+color[3]='puple'
+color[4]="purple"
+print(color)
