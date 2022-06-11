78742fc3b7f9ad1d34ef7dafaaf0cb19d30e01fb
Ashish bharti <98837841+ashishbhaarti4005@users.noreply.github.com>
2022-06-11 22:32:54 +0530

    Add files via upload

---
+++ assigment 4/output.py
@@ -0,0 +1,29 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/ashis/OneDrive/Desktop/assigment 4/code 1.py ========
+Enter Marks :88
+ Grade A 
+Enter a year: 2034
+year is not a Leap Year
+Question 1:2X3=6
+Right!
+Question 2:8X10=80
+Right!
+Question 3:2X10=20
+Right!
+Question 4:3X6=18
+Right!
+Question 5:5X4=20
+Right!
+Question 6:7X4=28
+Right!
+Question 7:9X4=32
+Wrong.The right answer is 36
+Question 8:9X1=9
+Right!
+Question 9:9X6=46
+Wrong.The right answer is 54
+Question 10:6X5=30
+Right!
+177 candies are in the bowl!
