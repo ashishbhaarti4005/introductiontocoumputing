2d13bb3260a6d4290f339e56a10de22ed4643357
Ashish bharti <98837841+ashishbhaarti4005@users.noreply.github.com>
2022-06-18 19:51:51 +0530

    Add files via upload

---
+++ assigment 5/output assg 5.py
@@ -0,0 +1,97 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/ashis/OneDrive/Desktop/assigment 5/output.py ========
+Enter the the string :physical
+lacisyhp
+Enter the minimum value :3
+Enter the maximum value: 90
+Enter the number for divisibility check :6
+6
+12
+18
+24
+30
+36
+42
+48
+54
+60
+66
+72
+78
+84
+90
+Enter the length of side:5
+Enter the length of side:7
+Enter the length of side:8
+17.320508075688775
+Enter the no. of rows :9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+Enter the number of rows5
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+Enter the Lowest Range Value : 1
+Enter the Upper Range Value : 80
+The Prime Numbers in this range are : 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+Enter the number 56
+Enter the number 98
+Enter the number -76
+Enter the number 55
+Enter the number 56
+Enter the number -90
+Enter the number 987
+Enter the number 21
+Enter the number 28
+Enter the number -089
+Positive numbers are:  [56, 98, 55, 56, 987, 21, 28]
+Negative numbers are:  [-76, -90, -89]
+Even numbers are:  [56, 98, -76, 56, -90, 28]
+Odd numbers are:  [55, 987, 21, -89]
+Number of times each number occurs in the List {56: 2, 98: 1, -76: 1, 55: 1, -90: 1, 987: 1, 21: 1, 28: 1, -89: 1}
+Number of words: 9
+Enter the word: sky
+Enter the word: sleep
+Enter the word: fly
+Enter the word: flag
+Enter the word: sky
+Enter the word: sky
+Enter the word: ant
+Enter the word: ear
+Enter the word: year
+Number of occurences:  {'sky': 3, 'sleep': 1, 'fly': 1, 'flag': 1, 'ant': 1, 'ear': 1, 'year': 1}
